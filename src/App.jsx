fadd6e4651d85a0a2bb15e9741444dbfb01d1ab1
Beata Debinska <bea.debinska@gmail.com>
2022-01-17 12:17:29 +0100

    Add notes to the app

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,26 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "./notes";
+
+function createNote(note) {
+  return (
+    <Note
+      key={note.key}
+      title={note.title}
+      content={note.content}
+    />
+  );
+}
 
 function App() {
-  return(
-    <div>
-      <Header />
-      <Note />
-      <Footer />
-    </div>
-  );
+ return (
+   <div>
+     <Header />
+     {notes.map(createNote)}
+     <Footer />
+   </div>
+ );
 }
 
 export default App;
